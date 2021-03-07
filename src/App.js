2bd56b0dd7375e8b2583e067b52925de8fc5fb1d
Gagandeep Singh <singh@Gagandeeps-MacBook-Pro.local>
2021-03-06 16:30:31 -0800

    setting up initial files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+    <h1>Welcome to the Home Page</h1>
+    <h3>This is where we render different pages of projects</h3>
+    </div>
+  );
+}
+
+export default App;
