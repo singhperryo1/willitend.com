35831ce8be18896e5322a2aaa1fe9338d506aa00
lihing1994 <li_hing@student.smc.edu>
2021-03-08 15:58:30 -0800

    Adding Dropdown Box

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import Dropdown from './Dropdown.js'
+export default function Home(){
+    return(
+        <div>
+        <h1>Welcome to the Home Page</h1>
+        <h3>This is where we render different pages of projects</h3>
+        <Dropdown/> 
+        </div>
+    );
+}
