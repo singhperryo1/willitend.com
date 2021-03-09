273a2af0517e3c5fad40740214da77d860a31ef3
DicksonMinang <mnngdickson2@yahoo.com>
2021-03-08 16:31:41 -0800

    added navbar

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import Navbar from './Navbar.js';
+export default function Home(){
+    return(
+        <div>
+        <h1>Welcome to the Home Page</h1>
+        <h3>This is where we render different pages of projects</h3>
+        <Navbar/>
+           </div>
+    );
+}
