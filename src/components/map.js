42c9c061d25a43e39eecd80b2ddb0fd8d5c724c4
Gagandeep Singh <singh@Gagandeeps-MacBook-Pro.local>
2021-03-11 19:39:44 -0800

    initial merging

--- src/components/map.js
+++ src/components/map.js
@@ -1,3 +1,4 @@
+import './Map.css';
 import React, { Component } from 'react';
 import USAMap from "react-usa-map";
 
@@ -5,13 +6,43 @@ import USAMap from "react-usa-map";
 class Map extends Component {
 
   mapHandler = (event) => {
-    alert("State: " + event.target.dataset.name);
+
+    const state = event.target.dataset.name; 
+
+    console.log(event.target.dataset);
+
+    if (state === "CA") {
+      console.log("The data is for " + state + "is : xyz")
+    } else {
+      alert("State: " + state)
+    }
+
+  };
+
+  statesCustomConfig = () => {
+
+    /*
+    Comments: 
+    fill color according to the data + state
+    Hover w/ boundaries + data 
+    avoid onClick events 
+    */
+
+    return {
+      "NJ": {
+        fill: "navy",
+        clickHandler: (event) => console.log('Custom handler for NJ', event.target.dataset)
+      },
+      "NY": {
+        fill: "#CC0000"
+      }
+    };
   };
  
   render() {
     return (
       <div className="Map">
-        <USAMap onClick={this.mapHandler} />
+        <USAMap customize = {this.statesCustomConfig()} onClick = {this.mapHandler} />
       </div>
     );
   }
