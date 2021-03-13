6d6d9a2dcd40b3fc5fd2279db73e66c1c6575f25
Gagandeep Singh <singh@Gagandeeps-MacBook-Pro.local>
2021-03-13 07:45:17 -0800

    made map responsive and added state names

--- src/components/map.js
+++ src/components/map.js
@@ -1,51 +1,81 @@
-import './Map.css';
-import React, { Component } from 'react';
-import USAMap from "react-usa-map";
+import React from "react";
+import { geoCentroid } from "d3-geo";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+  Annotation
+} from "react-simple-maps";
 
+import allStates from "./allstates.json";
 
-class Map extends Component {
+const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
 
-  mapHandler = (event) => {
+const offsets = {
+  VT: [50, -8],
+  NH: [34, 2],
+  MA: [30, -1],
+  RI: [28, 2],
+  CT: [35, 10],
+  NJ: [34, 1],
+  DE: [33, 0],
+  MD: [47, 10],
+  DC: [49, 21]
+};
 
-    const state = event.target.dataset.name; 
+const handleClick = (event) => {
+  console.log(event.target);
+  //alert("The state clicked is : " + event.target.id);
+}; 
 
-    console.log(event.target.dataset);
+const Map = () => {
+  return (
+    <ComposableMap projection="geoAlbersUsa">
+      <Geographies onClick = {handleClick} geography={geoUrl}>
+        {({ geographies }) => (
+          <>
+            {geographies.map(geo => (
+              <Geography
+                key={geo.rsmKey}
+                stroke="#FFF"
+                geography={geo}
+                fill="#DDD"
+              />
+            ))}
+            {geographies.map(geo => {
+              const centroid = geoCentroid(geo);
+              const cur = allStates.find(s => s.val === geo.id);
+              return (
+                <g key={geo.rsmKey + "-name"}>
+                  {cur &&
+                    centroid[0] > -160 &&
+                    centroid[0] < -67 &&
+                    (Object.keys(offsets).indexOf(cur.id) === -1 ? (
+                      <Marker coordinates={centroid}>
+                        <text y="2" fontSize={14} textAnchor="middle">
+                          {cur.id}
+                        </text>
+                      </Marker>
+                    ) : (
+                      <Annotation
+                        subject={centroid}
+                        dx={offsets[cur.id][0]}
+                        dy={offsets[cur.id][1]}
+                      >
+                        <text x={4} fontSize={14} alignmentBaseline="middle">
+                          {cur.id}
+                        </text>
+                      </Annotation>
+                    ))}
+                </g>
+              );
+            })}
+          </>
+        )}
+      </Geographies>
+    </ComposableMap>
+  );
+};
 
-    if (state === "CA") {
-      console.log("The data is for " + state + "is : xyz")
-    } else {
-      alert("State: " + state)
-    }
-
-  };
-
-  statesCustomConfig = () => {
-
-    /*
-    Comments: 
-    fill color according to the data + state
-    Hover w/ boundaries + data 
-    avoid onClick events 
-    */
-
-    return {
-      "NJ": {
-        fill: "navy",
-        clickHandler: (event) => console.log('Custom handler for NJ', event.target.dataset)
-      },
-      "NY": {
-        fill: "#CC0000"
-      }
-    };
-  };
- 
-  render() {
-    return (
-      <div className="Map">
-        <USAMap customize = {this.statesCustomConfig()} onClick = {this.mapHandler} />
-      </div>
-    );
-  }
-}
- 
 export default Map;
