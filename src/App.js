9e0f99cdf0ac5433db2a3c9b3ca013832f2aa269
DicksonMinang <mnngdickson2@yahoo.com>
2021-03-15 15:13:39 -0700

    Added a subscription button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Home from "./components/Home.js"; 
+import Subscription from "./components/Subscription.js"
 
 function App() {
   return (
     <div>
     <Home />
+    <Subscription />
     </div>
   );
 }
 
 export default App;
+
