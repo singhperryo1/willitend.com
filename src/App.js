29839e0eb2cad24236b489d66ec7a57a51a90e4e
DicksonMinang <mnngdickson2@yahoo.com>
2021-03-28 05:44:53 -0700

    Added the anectdotes page to be populated by user experiences

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import Home from "./components/Home.js"; 
+import Home from "./components/Home.js";
+import Anectdotes from "./components/Anectdotes.js"
 
 function App() {
   return (
     <div>
     <Home />
+    <Anectdotes />
     </div>
   );
 }
 
 export default App;
-
