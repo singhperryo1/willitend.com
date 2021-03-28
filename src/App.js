b2f8f395e9ba455acc7879613b0843c6da4fcc59
JaredTruston <jaredtruston@gmail.com>
2021-03-27 22:54:39 -0700

    Added in text boxes for default post information and text box + button for comment section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Home from "./components/Home.js"; 
+import Post_Default from "./components/Post_Default.js";
 
 function App() {
   return (
     <div>
-    <Home />
+    <Home/>
     </div>
   );
 }
---
+++ src/components/Post_Default.js
@@ -0,0 +1,33 @@
+import Comment from './Comment';
+import React from 'react';
+class Post_Default extends React.Component {
+	constructor(props){
+		super(props);
+		this.title = {data: "Sample Title"}
+		this.author = {data: "Sample Author"}
+		this.vaccine_site = {data: "Sample Vaccine-Site"}
+		this.date = {data: "Sample Date"}
+		this.post_content = {data: "Sample Post Content"}
+		this.state = {comments: []}
+	}
+
+  render() {
+    return (
+    	<div>
+        	<input type="text" value = {this.title.data} />
+        	<input type="text" value = {this.author.data} />
+        	<input type="text" value = {this.vaccine_site.data} />
+        	<input type="text" value = {this.date.data} />
+        	<br />
+        	<br />
+        	<div>
+        		<textarea type="textarea" value={this.post_content.data} maxlength="1000" cols="100" rows="20"></textarea>
+        	</div>
+        	Comments
+        	<Comment/>
+        </div>
+    );
+  }
+}
+
+export default Post_Default;
